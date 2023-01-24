0e850192545db120fff1a9c98655b067d627b19f
Vivek Baruah <vivekbrh01@gmail.com>
2023-01-24 20:48:37 +0530

    Add packages and header

---
+++ app/components/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+import Header from './Header/Header';
+
+export default function main() {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+}
